1ff175ca6ceb52456338cd535e20f165dc65decc
dain6415 <ekdlscjs4@naver.com>
2024-10-31 11:11:22 +0900

    1031

---
+++ C1/script/index.js
@@ -0,0 +1,48 @@
+// menu
+$('li.main-m').mouseenter(function(){
+  $(this).find('.sub-m').stop().slideDown()
+})
+$('li.main-m').mouseleave(function(){
+  $(this).find('.sub-m').stop().slideUp()
+})
+
+// slide
+$('.slide').eq(0).siblings().hide() /* 변수설정 */
+let idx = 0
+
+setInterval(function(){
+  idx ++
+
+  if(idx === 3){
+    idx = 0
+  }
+  $('.slide').fadeOut()
+  $('.slide').eq(idx).fadeIn()
+},3000)
+// tab
+$('.tablist ul li').click(function(){
+  $('.tablist ul li').removeClass('on')
+  $(this).addClass('on')
+
+  let idx = $(this).index()
+
+  $('.tab').hide()
+  $('.tab').eq(idx).show()
+})
+// $('.tablist ul li').click(function(){
+//   $('.tablist ul li').removeClass('on')
+//   $(this).addClass('on')
+
+//   let idx = $(this).index() /* 변수 */
+
+//   $('.tab').hide()
+//   $('.tab').eq(idx).show()
+// })
+
+// popup
+$('.modal').click(function(){
+  $('.popup').fadeIn()
+})
+$('.close').click(function(){
+  $('.popup').fadeOut()
+})
